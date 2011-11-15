da9edf14bb560ec5420ad46d1f32353510067466
Martin Tůma <tumic@cbox.cz>
2011-11-15 19:27:05 +0100

    Version 0.8

--- CORE/config.h
+++ CORE/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H_
 
 #define APP_HOMEPAGE			"http://tumic.wz.cz/hypercube"
-#define APP_VERSION				"0.7"
+#define APP_VERSION				"0.8"
 
 #define FONT_FAMILY				"Helvetica"
 #define AVG_CHAR_WIDTH			0.66f
