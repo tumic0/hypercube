56bac15e272f2b6f504d3e075f3c416f172e9d8f
Martin Tůma <tumic@cbox.cz>
2013-04-14 21:11:21 +0200

    Version 1.3.0

--- CORE/config.h
+++ CORE/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H_
 
 #define APP_HOMEPAGE			"http://tumic.wz.cz/hypercube"
-#define APP_VERSION				"1.2.0"
+#define APP_VERSION				"1.3.0"
 
 #define FONT_FAMILY				"Helvetica"
 #define AVG_CHAR_WIDTH			0.58f
