26c46016864022a0829eb5e0c6450c173436ee1a
Martin Tůma <tumic@cbox.cz>
2013-05-08 18:38:23 +0200

    Fixed lexstream error state handling

--- IO/io.h
+++ IO/io.h
@@ -56,14 +56,27 @@ class lexstream : public std::wifstream
 {
 public:
 	lexstream() : _unget(false) {}
-	int get() {
-		if (_unget)
-			_unget = false;
-		else
-			_last = std::wifstream::get();
-		return std::wifstream::good() ? _last : -1;
+	void open(const char *filename, std::ios_base::openmode mode =
+	  std::ios_base::in) {
+		_unget = false;
+		std::wifstream::open(filename, mode);
 	}
-	void unget() {_unget = true;}
+	int get() {
+		if (_unget) {
+			if (is_open())
+				_unget = false;
+			else
+				setstate(std::ios_base::badbit);
+		} else
+			_last = std::wifstream::get();
+		return good() ? _last : -1;
+	}
+	void unget() {
+		if (is_open())
+			_unget = true;
+		else
+			setstate(std::ios_base::badbit);
+	}
 
 private:
 	int _last;
